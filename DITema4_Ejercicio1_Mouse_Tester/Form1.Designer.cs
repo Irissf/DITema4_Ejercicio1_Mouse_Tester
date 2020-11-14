125d97c72329465ed0859b16c1728d495666e248
Irissf <iris.seijo@gmail.com>
2020-11-14 17:53:29 +0100

    Agregar archivos de proyecto.

---
+++ DITema4_Ejercicio1_Mouse_Tester/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace DITema4_Ejercicio1_Mouse_Tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(59, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(263, 115);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Izquierda";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.coordenadasBotones);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(342, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(263, 115);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Derecha";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.coordenadasBotones);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 418);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mouse Tester";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Segurito);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Prueba);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.cklickraton);
+            this.MouseLeave += new System.EventHandler(this.AbandonarForm);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.coordenadas);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.previewTecla);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DITema4_Ejercicio1_Mouse_Tester/Form1.cs
@@ -0,0 +1,114 @@
+﻿#define VALOR
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DITema4_Ejercicio1_Mouse_Tester
+{
+    public partial class Form1 : Form
+    {
+        public int x;
+        public int y;
+        bool teclas = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void coordenadas(object sender, MouseEventArgs e)
+        {
+            if (!teclas)
+            {
+                this.x = e.X;
+                this.y = e.Y;
+                this.Text = String.Format("Coordenadas X:{0}-Y:{1}", x, y);
+            }
+            
+        }
+
+        private void coordenadasBotones(object sender, MouseEventArgs e)
+        {
+
+            if (!teclas)
+            {
+                Button boton = (Button)sender;
+                this.x = boton.Location.X + e.X;
+                this.y = boton.Location.Y + e.Y;
+                this.Text = String.Format("Coordenadas X:{0}-Y:{1}", x, y);
+            }
+            
+
+        }
+
+        private void AbandonarForm(object sender, EventArgs e)
+        {
+            if (!teclas)
+            {
+                this.Text = "Mouse Tester";
+            }
+        }
+
+        private void cklickraton(object sender, MouseEventArgs e)
+        {
+            this.button1.BackColor = default;
+            this.button2.BackColor = default;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                this.button1.BackColor = Color.Beige;
+            }
+            else if(e.Button == MouseButtons.Right)
+            {
+                this.button2.BackColor = Color.Coral;
+            }else
+            {
+                this.button1.BackColor = Color.Aqua;
+                this.button2.BackColor = Color.Aqua;
+            }
+        }
+
+        private void previewTecla(object sender, PreviewKeyDownEventArgs e)
+        {
+            Console.WriteLine("fuck");
+            teclas = true;
+            this.Text = e.KeyCode.ToString();
+            if(e.KeyCode == Keys.Escape)
+            {
+                teclas = false;
+            }
+        }
+
+        private void Prueba(object sender, KeyEventArgs e)
+        {
+
+            teclas = true;
+#if VALOR
+            this.Text = e.KeyValue.ToString();
+#else
+            this.Text = e.KeyCode.ToString();
+#endif
+
+            if (e.KeyCode == Keys.Escape)
+            {
+                teclas = false;
+            }
+        }
+
+        private void Segurito(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("¿Segurito qué quieres salir?","Aplicación",
+                MessageBoxButtons.OKCancel,MessageBoxIcon.Question)==DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
